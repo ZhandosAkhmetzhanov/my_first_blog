9470e93bda24aa351a12ff97da29b0aa8af3bfef
ZhandosAkhmetzhan <jando.slon92@gmail.com>
2019-01-30 20:56:31 +0600

    Hello

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
-from . import views
+
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('', include('blog.urls')),
-path('', views.post_list, name='post_list'),
 ]
