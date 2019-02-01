61757063f2aad9a94ef3fc1b50e6868031d7813a
ZhandosAkhmetzhan <jando.slon92@gmail.com>
2019-02-01 20:59:30 +0600

    gggg

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.homepage, name='homepage'),
-path('gallery', views.gallery, name="gallery"),
+path('gallerry', views.gallery, name="gallery"),
 path('registration', views.registration, name="registration"),
 path('calculator', views.calculator, name="calculator"),
 ]
